f85ef6962655657c03d6173d8b9896130ca2deb8
_ranjeet <72652390+runj33t@users.noreply.github.com>
2023-02-03 12:31:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      This is just a react template, so you dont have to do npx create-react-app everytime 🙂
+    </div>
+  )
+}
+
+export default App
